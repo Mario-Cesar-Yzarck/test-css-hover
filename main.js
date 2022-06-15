21f900080c4635eaed0db0750f03df084dfe3f79
Mario <mc.fialho@hotmail.com>
2022-06-15 16:25:12 -0300

    first

---
+++ main.js
@@ -0,0 +1,6 @@
+const span1 = document.getElementById('1')
+const span2 = document.getElementById('2')
+const span3 = document.getElementById('3')
+
+const spanImg = document.getElementById('img')
+
